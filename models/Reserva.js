590dda705f563c467f1bd86c4fd1a4537013c682
Barboza Daira <daiyanela2008@gmail.com>
2023-07-05 15:01:56 -0300

    Se realizo el modelo de datos para las reservas

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,79 @@
 // TODO: Crear modelo de datos de Reserva
+const { sequelize, DataTypes } = require("../database");
+
+const Reserva = sequelize.define(
+  "Reserva",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    codigo: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    fecha_reserva: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    fecha_salida: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    fecha_llegada: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    origen: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    destino: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updateAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    deleteAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+  },
+  {
+    createAt: true,
+    updateAt: true,
+    deleteAt: true,
+    tableName: "reservas",
+  }
+);
